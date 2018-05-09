e843569ada43eb5f37d9cb33123208a9d05ad24e
Lizzy <lizzy.presland@gmail.com>
2018-05-08 18:52:02 -0700

    Working on conversational functionality; will add sample conversations to brainstorm better control flow

--- lightkeeper/topics/RecordConcern.ts
+++ lightkeeper/topics/RecordConcern.ts
@@ -2,7 +2,7 @@ import { Topic } from "botbuilder-topical";
 
 export class RecordConcern extends Topic {
     async onStart() {
-        await this.send("recording concern...");
+        await this.send("Great! Tell me about your concerns.");
         this.end();
     }
 
--- lightkeeper/topics/Root.ts
+++ lightkeeper/topics/Root.ts
@@ -4,21 +4,41 @@ import { RecordConcern } from ".";
 export class Root extends Topic {
 
     async onStart() {
-        await this.send("Would you like to record a concern or inquire about insights?");
+        await this.send("Hi, I'm Lightkeeper, and I'm here to help you help a child. Would you like to profile a student?");
     }
 
     async onDispatch() {
-        if (await this.dispatchToChild())
+        if (await this.dispatchToChild()) {
             return;
-
-        if (this.text === "record concern") {
-            await this.startChild(RecordConcern);
+        } else {
+            var input: string = `${this.text}`;
+            this.send(`You entered ${input}`);
+            if (this.findBehaviors(input) == true) {
+                this.send("Great! I'm adding this behavior to your list.");
+                await this.startChild(RecordConcern);
+            } else {
+                this.send("That behavior isn't part of our standard set of problematic behaviors, but I will add it to the list.");
+                await this.startChild(RecordConcern);
+            }
         }
     }
 
     async onChildEnd() {
         await this.send(`Welcome back to the Root!`);
     }
+    
+    findBehaviors(input: string) {
+        var i;
+        this.send(`Checking our index of ${behaviors.length - 1} behaviors`);
+        for (i = 0; i < behaviors.length; i++) {
+            if (input.includes(behaviors[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
 }
 
 Root.register();
+
+var behaviors = ["bully", "bullying", "violent", "violence", "abuse", "sad", "depressed", "bad grades", "undereating"];
