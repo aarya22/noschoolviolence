11d03ff88c81df6ee6cfe4d19486b3a2de25321c
booellean <booelle.commissions@gmail.com>
2018-05-08 22:17:30 -0700

    added import fetch etc to dependencies. Code for onStart is nearly fleshed out. Need to start JSONserver to verify that json can be GET and worked with to eventually compare.

--- lightkeeper/topics/Insights.ts
+++ lightkeeper/topics/Insights.ts
@@ -1,15 +1,35 @@
 import { Topic } from "botbuilder-topical";
+import { fetch } from "node-fetch";
 
 export class Insights extends Topic {
-    async onStart() {
-        await this.send("The concerning behavior has been added to my profile for this session. Feel free to keep adding concerns.");
-        this.end();
+    async onStart(concernSet) {
+        const baseURL = ;
+        fetch (baseURL, {
+            method: 'GET',
+            body: JSON.stringify(body),
+            headers: {'Content-Type' : 'application/JSON'},
+        }).then( res => {
+            console.log(res);
+        })
+        //The code here will check if there are any patterns in concern sets
+
+        //gather every set (sent as an array of JSONS) and send to Dispatch
+    }
+    //if concern sets match less than 50%, return
+            //if concern sets match at least 50%, ask user if the child is also exhibiting these behaviors
+
+    async onDispatch(concernBehavior) {
+        
+                //FOR LOOP (probably?) - if the user answers yes, POST the behavior to the student concerns list
+                //if user answers no, return
+            //if all data is no, send original set,
+
+            //if data has been added, send 
     }
 
-    async onDispatch() {
-    }
-
-    async onChildEnd() {
+    async onChildEnd(finalConcernSet) {
+        //Send all data to the Machine Learning Model
+        console.log(finalConcernSet);
     }
 
 }
