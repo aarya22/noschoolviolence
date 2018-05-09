42f7bae180d99e8ebf828f65ef57268c8851e484
Lizzy <lizzy.presland@gmail.com>
2018-05-09 11:59:34 -0700

    restructuring conversation so RecordConcern handles the bulk of the behavior logic

--- lightkeeper/topics/RecordConcern.ts
+++ lightkeeper/topics/RecordConcern.ts
@@ -2,16 +2,62 @@ import { Topic } from "botbuilder-topical";
 
 export class RecordConcern extends Topic {
     async onStart() {
-        await this.send("The concerning behavior has been added to my profile for this session. Feel free to keep adding concerns.");
-        this.end();
+        await this.send("Please add all your concerns below, each separated by a comma.")
+        
+        
+
     }
 
     async onDispatch() {
+        
+        var input: string = `${this.text}`;
+        if (this.endBehaviorEntry(input) == true) {
+            await this.send("It looks like you are done adding traits for this student -- thanks!");
+        } else {
+            this.send(`You entered ${input}`);
+            if (this.findBehaviors(input) == true) {
+                this.send("Great! I'm adding this behavior to your list.");
+                await this.startChild(RecordConcern);
+            } else {
+                this.send("That behavior isn't part of our standard set of problematic behaviors, but I will add it to the list.");
+                await this.startChild(RecordConcern);
+            }
+        }
+         
     }
 
     async onChildEnd() {
     }
 
+    
+    findBehaviors(input: string) {
+        var i;
+        this.send(`Checking our index of ${behaviors.length - 1} behaviors`);
+        for (i = 0; i < behaviors.length; i++) {
+            if (input.includes(behaviors[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+    
+    endBehaviorEntry(input: string) {
+        var i;
+        for (i = 0; i < doneWithBehaviorEntry.length; i++) {
+            if (input.includes(doneWithBehaviorEntry[i])) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+
 }
 
 RecordConcern.register();
+
+var behaviors = ["bullying", "violent", "depressed", "sad", "angry"]
+var doneWithBehaviorEntry = ["all done", "done", "end", "stop"]
+
+var behaviors = ["bully", "bullying", "violent", "violence", "abuse", "sad", "depressed", "bad grades", "undereating"];
+var doneWithBehaviorEntry = ["done", "finished", "move on", "stop", "end"];
--- lightkeeper/topics/Root.ts
+++ lightkeeper/topics/Root.ts
@@ -13,56 +13,23 @@ export class Root extends Topic {
         /**if (await this.dispatchToChild){
             return;
         }**/
+        if (this.context.activity.type !== 'message') {
+            return;
+        }
         if (this.context.activity.text === "yes"){
-            await this.send("Please add all your concerns below, each separated by a comma.")
+
+            await this.startChild(RecordConcern);
         }
         
-       /*  var input: string = `${this.text}`;
-        if (await this.dispatchToChild()) {
-            return;
-        } else if (this.endBehaviorEntry(input) == true) {
-            await this.send("It looks like you are done adding traits for this student -- thanks!");
-        } else {
-            this.send(`You entered ${input}`);
-            if (this.findBehaviors(input) == true) {
-                this.send("Great! I'm adding this behavior to your list.");
-                await this.startChild(RecordConcern);
-            } else {
-                this.send("That behavior isn't part of our standard set of problematic behaviors, but I will add it to the list.");
-                await this.startChild(RecordConcern);
-            }
-        } */
+
     }
 
     async onChildEnd() {
         await this.send(`Please add another concern, or let me know if you are finished.`);
     }
     
-    findBehaviors(input: string) {
-        var i;
-        this.send(`Checking our index of ${behaviors.length - 1} behaviors`);
-        for (i = 0; i < behaviors.length; i++) {
-            if (input.includes(behaviors[i])) {
-                return true;
-            }
-        }
-        return false;
-    }
-    
-    endBehaviorEntry(input: string) {
-        var i;
-        for (i = 0; i < doneWithBehaviorEntry.length; i++) {
-            if (input.includes(doneWithBehaviorEntry[i])) {
-                return true;
-            }
-        }
-        return false;
-    }
 
 
 }
 
 Root.register();
-
-var behaviors = ["bully", "bullying", "violent", "violence", "abuse", "sad", "depressed", "bad grades", "undereating"];
-var doneWithBehaviorEntry = ["done", "finished", "move on", "stop", "end"];
